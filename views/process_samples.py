1d0138a7aa6bc9b3f67ba5722aa4f96136467887
afonsolsj <84239743+afonsolsj@users.noreply.github.com>
2025-11-22 19:03:58 -0300

    Correção

--- views/process_samples.py
+++ views/process_samples.py
@@ -284,8 +284,7 @@ def extract_fields_positive(report_text, df_name):
         other_micro = isolate_micro if type_micro == "" and isolate_micro else ""
         mechanism = "", ""
         code_mcim, code_ecim = get_cim_result(report_text) if mechanism in [1, 3] else ("", "")
-        fluconazol, voriconazol, caspofungina, micafungina, anfotericina_b, fluocitosina = code_ats(get_value("fluconazol").split("-")[0].strip()), code_ats(get_value("voriconazol").split("-")[0].strip()),
-        code_ats(get_value("caspofungina").split("-")[0].strip()), code_ats(get_value("micafungina").split("-")[0].strip()), code_ats(get_value("anfotericina_b").split("-")[0].strip()), code_ats(get_value("fluocitosina").split("-")[0].strip())
+        fluconazol, voriconazol, caspofungina, micafungina, anfotericina_b, fluocitosina = code_ats(get_value("fluconazol").split("-")[0].strip()), code_ats(get_value("voriconazol").split("-")[0].strip()), code_ats(get_value("caspofungina").split("-")[0].strip()), code_ats(get_value("micafungina").split("-")[0].strip()), code_ats(get_value("anfotericina_b").split("-")[0].strip()), code_ats(get_value("fluocitosina").split("-")[0].strip())
         return {
             "resultado": 1,
             "qual_microorganismo": micro_final,
