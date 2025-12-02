9d23e932bd67ffab194971e1e3588b68416ee0b6
Afonso Saraiva <84239743+afonsolsj@users.noreply.github.com>
2025-12-02 21:04:37 +0000

    Diagnostico

--- views/process_samples.py
+++ views/process_samples.py
@@ -385,6 +385,7 @@ def extract_fields_positive(report_text, df_name):
         def get_gn_hospitalar_values(get_value, result_ast, report_lower, type_micro):
             campos = ["amoxicilina", "aztreonam", "cefiderocol", "ceftalozano/tazobactam", "ceftazidima/avibactam", "ampicilina", "ampicilina/sulbactam", "piperacilina/tazobactam", "cefoxitina", "cefuroxima", "ceftazidima", "cefepima", "ertapenem", "imipenem", "imipenem/relebactam", "levofloxacina", "meropenem", "meropenem/vaborbactam", "amicacina", "gentamicina", "ciprofloxacina", "tigeciclina", "trimetoprim/sulfametozol", "polimixina b", "ceftriaxona"]
             if "amb" not in get_value("Procedência.:") and type_micro == 1:
+                print(get_value("Procedência.:"))
                 valores = [result_ast(get_value(c)) for c in campos]
                 gram_negativo_gn_hospitala = 1
                 return (*valores, gram_negativo_gn_hospitala)
