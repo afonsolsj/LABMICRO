231cecc80f58a8dc5411bf91bb85fbe412b68605
Afonso Saraiva <84239743+afonsolsj@users.noreply.github.com>
2025-12-12 11:40:01 +0000

    Finalização do codigo (remoção de Amostras Negativas)

--- views/main.py
+++ views/main.py
@@ -2,8 +2,5 @@ import streamlit as st
 
 st.title("Estagiários Lab Microbiologia")
 st.markdown(f"Bem-vindo, **{st.session_state.username}** 👋")
-col1, col2 = st.columns(2)
-if col1.button("Amostras negativas"):
-    st.switch_page("views/negative.py")
-if col2.button("Compilação de amostras"):
+if st.button("Compilação de amostras"):
     st.switch_page("views/process_samples.py")
