a3ae3137d6c3f45d8220049a30274777b64f190d
Afonso Saraiva <84239743+afonsolsj@users.noreply.github.com>
2025-12-25 23:43:59 +0000

    Melhora da pagina inical

--- views/main.py
+++ views/main.py
@@ -9,14 +9,12 @@ REPO_NAME = "LABMICRO"
 FILE_PATH = "assets/files/notice_board.txt"
 API_URL = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/contents/{FILE_PATH}"
 
-# --- FUNÇÕES PARA O GITHUB ---
 def get_post_it_content():
     response = requests.get(API_URL, headers={"Authorization": f"token {GITHUB_TOKEN}"})
     if response.status_code == 200:
         content = response.json()
         return base64.b64decode(content['content']).decode('utf-8'), content['sha']
     return "", None
-
 def update_post_it_github(new_text, sha):
     content_encoded = base64.b64encode(new_text.encode('utf-8')).decode('utf-8')
     data = {
@@ -27,39 +25,28 @@ def update_post_it_github(new_text, sha):
     response = requests.put(API_URL, json=data, headers={"Authorization": f"token {GITHUB_TOKEN}"})
     return response.status_code == 200
 
-# --- INTERFACE ---
 st.title("Estagiários Lab Microbiologia")
 st.markdown(f"Bem-vindo, **{st.session_state.username}** 👋")
 
-# Criando colunas (2/3 para botões, 1/3 para o post-it)
-col1, col2 = st.columns([2, 1])
-
+col1, col2 = st.columns([1, 4])
 with col1:
     if st.button("Compilação de amostras", use_container_width=True):
         st.switch_page("views/process_samples.py")
     if st.button("Remoção de duplicatas", use_container_width=True):
         st.switch_page("views/remove_duplicate.py")
-
 with col2:
-    st.subheader("📌 Mural de Avisos")
-    
-    # Busca o conteúdo atual
+    st.markdown('<p style="font-size: 14px; margin-bottom: 5px;">📌 Mural de avisos</p>', unsafe_allow_html=True)
     current_text, sha = get_post_it_content()
-
-    # Estado de edição
     if "editing" not in st.session_state:
         st.session_state.editing = False
-
     if not st.session_state.editing:
-        # Modo Visualização
         st.info(current_text if current_text else "Nenhum aviso no momento.")
-        if st.button("Editar Post-it"):
+        if st.button("✏️"):
             st.session_state.editing = True
             st.rerun()
     else:
-        # Modo Edição
         new_content = st.text_area("Escreva o aviso:", value=current_text, height=150)
-        if st.button("Salvar no GitHub"):
+        if st.button("💾"):
             if update_post_it_github(new_content, sha):
                 st.success("Salvo!")
                 st.session_state.editing = False
